a025b8446207734c752e3eeff043a395f22a8fa4
kravchuk-st <qkp@ukr.net>
2022-08-22 18:51:58 +0300

    feat: add header

--- client/module.d.ts
+++ client/module.d.ts
@@ -1,4 +1,4 @@
-declare module "*.png";
-declare module "*.jpg";
-declare module "*.css";
-declare module "*.scss";
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.css';
+declare module '*.scss';
