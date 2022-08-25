e4b0b8262c3b33a57f9b9f4d40fa13b202c7784b
Philipp Khromov <filooloog@mail.ru>
2022-08-25 14:19:48 +0300

    feat: add recipesService on client

---
+++ client/src/config/config.js
@@ -0,0 +1,8 @@
+const BASE_URL = 'https://rs-recipes-server.herokuapp.com';
+
+const ENDPOINTS = {
+  recipes: '/recipes',
+  recipesDistinctProps: '/recipes/distinct',
+};
+
+export { BASE_URL, ENDPOINTS };
