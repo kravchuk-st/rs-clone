96efd4767168fbf20acc365713f6e3d8fbb3318e
Philipp Khromov <filooloog@mail.ru>
2022-08-26 18:46:21 +0300

    feat: add transformQueryToSelector to handle queries with options

--- server/utils/utils.js
+++ server/utils/utils.js
@@ -1,3 +1,6 @@
+const { PAGE_NUMBER, ITEMS_PER_PAGE } = require('../general/config');
+const queryMap = require('../general/queryMap');
+
 const capitalizePath = (path) => {
   const pathSplitted = path.split('-');
   const pathProcessed = pathSplitted.map((pathPart, index) => {
@@ -9,4 +12,23 @@ const capitalizePath = (path) => {
   return pathProcessed.join('');
 };
 
-module.exports = capitalizePath;
+const transformQueryToSelector = (queryObject) => {
+  const selector = {
+    page: Number(queryObject.page) || PAGE_NUMBER,
+    limit: Number(queryObject.limit) || ITEMS_PER_PAGE,
+  };
+
+  for (let [property, value] of Object.entries(queryObject)) {
+    if (queryMap[property] === undefined) continue;
+
+    const [selectorProperty, selectorValue] = queryMap[property](value);
+    selector[selectorProperty] = selectorValue;
+  }
+
+  return selector;
+};
+
+module.exports = {
+  capitalizePath,
+  transformQueryToSelector,
+};
