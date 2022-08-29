61ae4966970eaf52f40883078dc31abe946b9505
Philipp Khromov <filooloog@mail.ru>
2022-08-29 16:17:08 +0300

    feat: add initial query options for breakfast section

--- client/module.d.ts
+++ client/module.d.ts
@@ -1,5 +1,6 @@
 declare module '*.ttf';
 declare module '*.png';
 declare module '*.jpg';
+declare module '*.svg';
 declare module '*.css';
 declare module '*.scss';
---
+++ client/src/pages/main/config.ts
@@ -0,0 +1,9 @@
+import { IQueryOptions } from '../../types';
+
+const initBreakfastQueryOptions: IQueryOptions = {
+  page: 0,
+  limit: 5,
+  'dish-types': ['morning meal', 'breakfast'],
+};
+
+export { initBreakfastQueryOptions };
