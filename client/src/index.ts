37548c1c7e2185d9b899bc3ccf1b5de53a325cc6
kravchuk-st <qkp@ukr.net>
2022-08-29 14:58:43 +0300

    refactor: change structure of the slider

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,18 +1,4 @@
 import './styles/main.scss';
-import Swiper from '../src/features/swiper.min';
+import { slider } from '../src/features/slider';
 
-new Swiper('.mySwiper', {
-  slidesPerView: 3,
-  spaceBetween: 40,
-  loop: true,
-  loopFillGroupWithBlank: true,
-  autoplay: {
-    delay: 3500,
-    stopOnLastSlide: false,
-    disableOnInteraction: false,
-  },
-  navigation: {
-    nextEl: '.swiper__btn_next',
-    prevEl: '.swiper__btn_prev',
-  },
-});
+slider;
