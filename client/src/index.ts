50571b2ca3101171d11055b8b67e825b297d7540
Alina <malinka775@yandex.ru>
2022-08-30 12:14:39 +0300

    feat: add ts to select element

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,5 +1,7 @@
 import { tabHandler } from './features/tabs';
 import './styles/main.scss';
 import './features/range-filters';
+import selectorHandler from './features/selector';
 
 tabHandler();
+selectorHandler();
