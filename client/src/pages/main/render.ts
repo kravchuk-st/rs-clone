31cf96e4045aa606153e1638d7585727924b0ad4
Philipp Khromov <filooloog@mail.ru>
2022-08-30 18:39:33 +0300

    feat: change card render function to render an anchor, implement recipe page script

--- client/src/pages/main/render.ts
+++ client/src/pages/main/render.ts
@@ -16,29 +16,31 @@ function renderRecipeCard(
   const cardElementClassNames = size === 'normal' ? classList.concat('card') : classList.concat(['card', 'card_lg']);
   const cardElement = createElementWithClass(elemType, ...cardElementClassNames);
   cardElement.innerHTML = `
-    <div class="card__header">
-      <p class="card__dish">${dishType}</p>
-      <object type="image/svg+xml" data=${heartImg} class="favorite"></object>
-    </div>
-    <img class="card__img" src=${recipeData.image} alt=${recipeData.title}>
-    <div class="card__meta">
-      <div class="data__time">
-        <p class="time__title">Cook time</p>
-        <p class="time__info">${recipeData.readyInMinutes} min</p>
+    <a href="./recipe.html?id=${recipeData.id}" id="${recipeData.id}">
+      <div class="card__header">
+        <p class="card__dish">${dishType}</p>
+        <object type="image/svg+xml" data=${heartImg} class="favorite"></object>
       </div>
-      <div class="data__calories">
-        <p class="calories__title">Calories</p>
-        <p class="calories__info">${calories} kcal</p>
+      <img class="card__img" src=${recipeData.image} alt=${recipeData.title}>
+      <div class="card__meta">
+        <div class="data__time">
+          <p class="time__title">Cook time</p>
+          <p class="time__info">${recipeData.readyInMinutes} min</p>
+        </div>
+        <div class="data__calories">
+          <p class="calories__title">Calories</p>
+          <p class="calories__info">${calories} kcal</p>
+        </div>
+        <div class="data__rating">
+          <p class="rating__title">Rating</p>
+          <p class="rating__info">${recipeData.aggregateLikes}</p>
+        </div>
       </div>
-      <div class="data__rating">
-        <p class="rating__title">Rating</p>
-        <p class="rating__info">${recipeData.aggregateLikes}</p>
+      <div class="card__body">
+        <h3 class="card__title">${recipeData.title}</h3>
+        <p class="card__text">${recipeData.summary}</p>
       </div>
-    </div>
-    <div class="card__body">
-      <h3 class="card__title">${recipeData.title}</h3>
-      <p class="card__text">${recipeData.summary}</p>
-    </div>
+    </a>
   `;
 
   return cardElement;
