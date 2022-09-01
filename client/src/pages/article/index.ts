9e4866e636d3e9ae9088d0c7f2a3d18ca0f5670a
Philipp Khromov <filooloog@mail.ru>
2022-09-01 20:29:08 +0300

    feat: add article loader

--- client/src/pages/article/index.ts
+++ client/src/pages/article/index.ts
@@ -1,1 +1,8 @@
 import '../../styles/main.scss';
+import loadArticle from './controller';
+
+const queryString = window.location.search;
+const params = new URLSearchParams(queryString);
+const articleId = params.get('id') as string;
+
+loadArticle(articleId);
