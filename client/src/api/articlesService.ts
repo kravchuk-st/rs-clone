c5cf05779cde20356b4de79aa5d15c5c29d5caff
Philipp Khromov <filooloog@mail.ru>
2022-09-01 16:35:52 +0300

    fix: articlesService formatting

--- client/src/api/articlesService.ts
+++ client/src/api/articlesService.ts
@@ -1,1 +1,24 @@
-import { BASE_URL, ENDPOINTS } from '../config/api.config';import { IArticle, IArticleQueryOptions } from '../types';import transformOptionsToQueryString from '../helpers/transformOptionsToQueryString';const getArticles = async (queryOptions?: IArticleQueryOptions): Promise<IArticle[]> => {  let queryString = '';  if (queryOptions && Object.keys(queryOptions).length !== 0) {    queryString = transformOptionsToQueryString(queryOptions as IArticleQueryOptions);  }  const response = await fetch(`${BASE_URL}${ENDPOINTS.articles}/${queryString}`);  return ((await response.json()) as unknown) as IArticle[];};const getArticlesById = async (articleId: number): Promise<IArticle> => {  const response = await fetch(`${BASE_URL}${ENDPOINTS.articles}/${articleId}`);  return ((await response.json()) as unknown) as IArticle;};export { getArticles, getArticlesById };
+import { BASE_URL, ENDPOINTS } from '../config/api.config';
+import { IArticle, IArticleQueryOptions } from '../types';
+
+import transformOptionsToQueryString from '../helpers/transformOptionsToQueryString';
+
+const getArticles = async (queryOptions?: IArticleQueryOptions): Promise<IArticle[]> => {
+  let queryString = '';
+
+  if (queryOptions && Object.keys(queryOptions).length !== 0) {
+    queryString = transformOptionsToQueryString(queryOptions as IArticleQueryOptions);
+  }
+
+  const response = await fetch(`${BASE_URL}${ENDPOINTS.articles}/${queryString}`);
+
+  return ((await response.json()) as unknown) as IArticle[];
+};
+
+const getArticlesById = async (articleId: number): Promise<IArticle> => {
+  const response = await fetch(`${BASE_URL}${ENDPOINTS.articles}/${articleId}`);
+
+  return ((await response.json()) as unknown) as IArticle;
+};
+
+export { getArticles, getArticlesById };
