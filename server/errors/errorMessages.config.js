b118e3b58e4851a1b3377979488e792e0b1afc91
Philipp Khromov <filooloog@mail.ru>
2022-09-01 14:46:07 +0300

    feat: add errorMessages config

---
+++ server/errors/errorMessages.config.js
@@ -0,0 +1,18 @@
+const errorMessages = {
+  general: {
+    notFound: '404 Not found',
+    mongoConnection: 'MongoDB connection error:',
+  },
+  recipes: {
+    invalidProperty: 'Invalid property path',
+    invalidId: 'Invalid recipe ID',
+    notFound: (recipeId) => `Recipe for id ${recipeId} not found`,
+    propertyNotFound: 'Provided recipe property path was not found',
+  },
+  articles: {
+    invalidId: 'Invalid article ID',
+    notFound: (articleId) => `Article for id ${articleId} not found`,
+  },
+};
+
+module.exports = errorMessages;
