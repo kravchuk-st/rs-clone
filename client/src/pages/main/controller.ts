8bfe11753650b81d2b9d3972009c2a002923471c
Philipp Khromov <filooloog@mail.ru>
2022-09-03 23:34:17 +0300

    feat: update main page controller (add form listeners)

--- client/src/pages/main/controller.ts
+++ client/src/pages/main/controller.ts
@@ -1,24 +1,23 @@
 import { renderRecipeCard, renderArticleCard } from './render';
-import * as recipesSerivice from '../../api/recipesService';
+import * as recipesService from '../../api/recipesService';
 import * as articlesService from '../../api/articlesService';
-import getCookieValue from '../../helpers/getCookieValue';
+import * as formHandler from '../../helpers/loginFormHandlers';
 
 import { ILoadArticleCard, ILoadRecipeCard, IArticle, IRecipe } from '../../types';
 import { recipesLoadConfig, articlesLoadConfig } from './config';
-import { BASE_URL, ENDPOINTS } from '../../config/api.config';
 
 async function loadMainPageContent() {
-  await loadContent(recipesLoadConfig.popular, recipesSerivice.getRecipes);
+  await loadContent(recipesLoadConfig.popular, recipesService.getRecipes);
   await loadContent(articlesLoadConfig, articlesService.getArticles);
-  await loadContent(recipesLoadConfig.breakfast, recipesSerivice.getRecipes);
-  await loadContent(recipesLoadConfig.lunch, recipesSerivice.getRecipes);
-  await loadContent(recipesLoadConfig.dinner, recipesSerivice.getRecipes);
-  await loadContent(recipesLoadConfig.bakery, recipesSerivice.getRecipes);
+  await loadContent(recipesLoadConfig.breakfast, recipesService.getRecipes);
+  await loadContent(recipesLoadConfig.lunch, recipesService.getRecipes);
+  await loadContent(recipesLoadConfig.dinner, recipesService.getRecipes);
+  await loadContent(recipesLoadConfig.bakery, recipesService.getRecipes);
 }
 
 async function loadContent(
   loadConfig: ILoadRecipeCard | ILoadArticleCard,
-  contentLoadingService: typeof articlesService.getArticles | typeof recipesSerivice.getRecipes
+  contentLoadingService: typeof articlesService.getArticles | typeof recipesService.getRecipes
 ) {
   const sectionContainer = document.querySelector(`.${loadConfig.containerClass}`) as HTMLElement;
   const sectionContainerList = sectionContainer.querySelector(`.${loadConfig.listClass}`) as HTMLUListElement;
@@ -41,29 +40,9 @@ function renderItems(itemsData: IArticle[] | IRecipe[], loadConfig: ILoadRecipeC
 }
 
 function addListeners() {
-  addUserButtonListener();
-  // addRegisterButtonListener();
-  // addSignInButtonListener();
-}
-
-function addUserButtonListener() {
-  const userProfileButton = document.querySelector('.profile-btn') as HTMLButtonElement;
-  const signupForm = document.querySelector('.popup') as HTMLElement;
-  const tokenValue = getCookieValue('token');
-
-  userProfileButton.addEventListener('click', async () => {
-    if (tokenValue) {
-      const response = await fetch(`${BASE_URL}${ENDPOINTS.userProfile}`);
-      if (response.status === 200) {
-        const userId = response.user.id;
-        window.open(`${window.location.host}/user-page.html/?id=${userId}`, '_self');
-      } else {
-        signupForm.classList.add('is-open');
-      }
-    } else {
-      signupForm.classList.add('is-open');
-    }
-  });
+  formHandler.addUserButtonListener();
+  formHandler.addRegisterFormListener();
+  formHandler.addSignInFormListener();
 }
 
 export { loadMainPageContent, addListeners };
