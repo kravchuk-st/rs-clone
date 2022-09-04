94ed165c9bcfdb5400a4562955d3c05a36fecf66
Philipp Khromov <filooloog@mail.ru>
2022-09-04 23:09:21 +0300

    feat: update cards styles & layout for correct anchor behaviour

--- client/src/features/renderCards.ts
+++ client/src/features/renderCards.ts
@@ -1,8 +1,6 @@
 import createElementWithClass from '../helpers/createElementWithClass';
 import capitalize from '../helpers/capitalize';
 import { IArticle, ILoadArticleCard, ILoadRecipeCard, IRecipe } from '../types';
-import * as articlesService from '../api/articlesService';
-import * as recipesService from '../api/recipesService';
 
 function renderRecipeCard(
   recipeData: IRecipe,
@@ -12,35 +10,42 @@ function renderRecipeCard(
 ): HTMLElement {
   const dishType = capitalize(recipeData.dishTypes[0]) || '';
   const calories = Math.round(recipeData.nutrition.nutrients[0].amount);
+  const cardLink = `./recipe.html?id=${recipeData.id}`;
 
   const cardElementClassNames = size === 'normal' ? classList.concat('card') : classList.concat(['card', 'card_lg']);
-  const cardElement = createElementWithClass(elemType, ...cardElementClassNames) as HTMLAnchorElement;
-  cardElement.href = `./recipe.html?id=${recipeData.id}`;
+  const cardElement = createElementWithClass(elemType, ...cardElementClassNames) as HTMLLIElement;
   cardElement.id = String(recipeData.id);
   cardElement.innerHTML = `
     <div class="card__header">
       <p class="card__dish">${dishType}</p>
-      <button class="favorite-article btn-reset favorite-btn"></button>
-    </div>
-    <img class="card__img" src=${recipeData.image} alt=${recipeData.title}>
-    <div class="card__meta">
-      <div class="data__time">
-        <p class="time__title">Cook time</p>
-        <p class="time__info">${recipeData.readyInMinutes} min</p>
-      </div>
-      <div class="data__calories">
-        <p class="calories__title">Calories</p>
-        <p class="calories__info">${calories} kcal</p>
-      </div>
-      <div class="data__rating">
-        <p class="rating__title">Rating</p>
-        <p class="rating__info">${recipeData.aggregateLikes}</p>
+      <div class="recipe__btns">
+        <button class="save-recipe btn-reset save-btn"></button>
+        <button class="favorite-recipe btn-reset favorite-btn"></button>
       </div>
     </div>
-    <div class="card__body">
-      <h3 class="card__title">${recipeData.title}</h3>
-      <p class="card__text">${recipeData.summary}</p>
-    </div>
+    <a class="card__img" href="${cardLink}">
+      <img class="card__img" src=${recipeData.image} alt=${recipeData.title}>
+    </a>
+    <a class="card-anchor" href=${cardLink}>
+      <div class="card__meta">
+        <div class="data__time">
+          <p class="time__title">Cook time</p>
+          <p class="time__info">${recipeData.readyInMinutes} min</p>
+        </div>
+        <div class="data__calories">
+          <p class="calories__title">Calories</p>
+          <p class="calories__info">${calories} kcal</p>
+        </div>
+        <div class="data__rating">
+          <p class="rating__title">Rating</p>
+          <p class="rating__info">${recipeData.aggregateLikes}</p>
+        </div>
+      </div>
+      <div class="card__body">
+        <h3 class="card__title">${recipeData.title}</h3>
+        <p class="card__text">${recipeData.summary}</p>
+      </div>
+    </a>
   `;
 
   return cardElement;
@@ -55,6 +60,7 @@ function renderArticleCard(articleData: IArticle, articleClass: string[]): HTMLE
   const articleLink = `./article.html?id=${articleData._id}`;
 
   const articleElement = createElementWithClass('article', ...articleClass) as HTMLElement;
+  articleElement.id = articleData._id;
   articleElement.innerHTML = `
     <img class="article__img" src=${articleData.image} alt="article preview">
     <div class="article__content">
