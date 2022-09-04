4d3767c564bedbde0d82ea5d086cfba81e5e2f5b
Philipp Khromov <filooloog@mail.ru>
2022-09-05 00:58:12 +0300

    feat: add save and favorite button listeners on main page

--- client/src/pages/main/controller.ts
+++ client/src/pages/main/controller.ts
@@ -1,10 +1,13 @@
 import loadCardsContent from '../../features/loadCards';
 import * as recipesService from '../../api/recipesService';
 import * as articlesService from '../../api/articlesService';
+import * as userService from '../../api/userService';
 import * as formHandler from '../../helpers/loginFormHandlers';
 
 import { recipesLoadConfig, articlesLoadConfig } from './config';
 import { renderCards } from '../../features/renderCards';
+import { IUserResponse } from '../../types';
+import { ENDPOINTS } from '../../config/api.config';
 
 async function loadMainPageContent() {
   const popularRecipes = await loadCardsContent(recipesLoadConfig.popular, recipesService.getRecipes);
@@ -30,6 +33,70 @@ function addListeners() {
   formHandler.addUserButtonListener();
   formHandler.addRegisterFormListener();
   formHandler.addSignInFormListener();
+  addArticleButtonsListeners();
+  addRecipeButtonsListeners();
+}
+
+function addArticleButtonsListeners() {
+  const articles = document.querySelectorAll('.article');
+  articles.forEach(article => {
+    const saveButton = article.querySelector('.save-btn') as HTMLButtonElement;
+    const favoriteButton = article.querySelector('.favorite-btn') as HTMLButtonElement;
+
+    saveButton.addEventListener('click', (e: Event) => {
+      const targetButton = e.target as HTMLButtonElement;
+      targetButton.classList.toggle('is-active');
+      const userObject = JSON.parse(localStorage.getItem('user') || 'null');
+      if (userObject) updateUserResources(userObject, article, targetButton, 'articles', 'saved');
+    });
+
+    favoriteButton.addEventListener('click', (e: Event) => {
+      const targetButton = e.target as HTMLButtonElement;
+      targetButton.classList.toggle('is-active');
+      const userObject = JSON.parse(localStorage.getItem('user') || 'null');
+      if (userObject) updateUserResources(userObject, article, targetButton, 'articles', 'favorite');
+    });
+  });
+}
+
+function addRecipeButtonsListeners() {
+  const recipes = document.querySelectorAll('.card');
+  recipes.forEach(recipe => {
+    const saveButton = recipe.querySelector('.save-btn') as HTMLButtonElement;
+    const favoriteButton = recipe.querySelector('.favorite-btn') as HTMLButtonElement;
+
+    saveButton.addEventListener('click', (e: Event) => {
+      const targetButton = e.target as HTMLButtonElement;
+      targetButton.classList.toggle('is-active');
+      const userObject = JSON.parse(localStorage.getItem('user') || 'null');
+      if (userObject) updateUserResources(userObject, recipe, targetButton, 'recipes', 'saved');
+    });
+
+    favoriteButton.addEventListener('click', (e: Event) => {
+      const targetButton = e.target as HTMLButtonElement;
+      targetButton.classList.toggle('is-active');
+      const userObject = JSON.parse(localStorage.getItem('user') || 'null');
+      if (userObject) updateUserResources(userObject, recipe, targetButton, 'recipes', 'favorite');
+    });
+  });
+}
+
+function updateUserResources(
+  userObject: IUserResponse,
+  cardElement: Element,
+  eventTarget: HTMLButtonElement,
+  cardName: 'articles' | 'recipes',
+  cardCategory: 'saved' | 'favorite'
+) {
+  if (eventTarget.classList.contains('is-active')) {
+    userObject[cardName][cardCategory].push(cardElement.id);
+  } else {
+    userObject[cardName][cardCategory] = userObject[cardName][cardCategory].filter(
+      resourceId => resourceId !== cardElement.id
+    );
+  }
+  userService.sendUserData(userObject, ENDPOINTS.userUpdate);
+  localStorage.setItem('user', JSON.stringify(userObject));
 }
 
 export { loadMainPageContent, addListeners };
