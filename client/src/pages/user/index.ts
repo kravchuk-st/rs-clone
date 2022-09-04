d0ea7563cf2d3cc05e482a1789a9d798c4f2fdf8
Alina <malinka775@yandex.ru>
2022-09-04 19:57:53 +0300

    feat: add list-item controls handler

--- client/src/pages/user/index.ts
+++ client/src/pages/user/index.ts
@@ -1,6 +1,9 @@
 import { burgerMenu } from '../../features/burgerMenu';
+import productListControlsHandler from '../../features/productListControlsHandler';
 import { tabHandler } from '../../features/tabs';
 import '../../styles/main.scss';
+import '../../features/productListInputHandler';
 
 burgerMenu();
 tabHandler();
+productListControlsHandler();
