498062596a331ad32e56261b69106137ec9c5149
Philipp Khromov <filooloog@mail.ru>
2022-09-06 03:20:20 +0300

    fix: add popup handler and tabHandler to all pages

--- client/src/pages/about/index.ts
+++ client/src/pages/about/index.ts
@@ -1,1 +1,8 @@
 import '../../styles/main.scss';
+import { tabHandler } from '../../features/tabs';
+import { getUserName } from '../../helpers/manageUserName';
+import popupHandler from '../../features/popup';
+
+getUserName();
+popupHandler();
+tabHandler('forms-container');
--- client/src/pages/recipe/index.ts
+++ client/src/pages/recipe/index.ts
@@ -2,6 +2,8 @@ import { burgerMenu } from '../../features/burgerMenu';
 import '../../styles/main.scss';
 import * as Controller from './controller';
 import { getUserName } from '../../helpers/manageUserName';
+import { tabHandler } from '../../features/tabs';
+import popupHandler from '../../features/popup';
 
 burgerMenu();
 const queryString = window.location.search;
@@ -10,3 +12,5 @@ const recipeId = params.get('id') as string;
 
 Controller.loadRecipe(recipeId).then(() => Controller.addListeners());
 getUserName();
+popupHandler();
+tabHandler('forms-container');
