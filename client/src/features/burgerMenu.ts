66e9574139b33d21052ec2150b99815712e8f730
Philipp Khromov <filooloog@mail.ru>
2022-09-06 14:58:23 +0300

    feat: add scroll block for burger menu

--- client/src/features/burgerMenu.ts
+++ client/src/features/burgerMenu.ts
@@ -6,12 +6,24 @@ export const burgerMenu = () => {
   burgerBtn.addEventListener('click', () => {
     burgerBtn.classList.toggle('burger_open');
     menu.classList.toggle('navbar__list_open');
+    burgerBtn.classList.contains('burger_open') ? disableScroll() : enableScroll();
   });
 
   menuLinks.forEach((el: HTMLElement) => {
     el.addEventListener('click', () => {
       burgerBtn.classList.remove('burger_open');
       menu.classList.remove('navbar__list_open');
+      enableScroll();
     });
   });
 };
+
+function disableScroll() {
+  document.body.style.overflow = 'hidden';
+  document.body.style.userSelect = 'none';
+}
+
+function enableScroll() {
+  document.body.style.overflow = 'auto';
+  document.body.style.userSelect = 'auto';
+}
