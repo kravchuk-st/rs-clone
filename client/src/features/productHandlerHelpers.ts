0ad371930324e6549a6bd18556ccf973a95e6ca3
Philipp Khromov <filooloog@mail.ru>
2022-09-15 12:58:22 +0300

    refactor: user profile management of product lists

--- client/src/features/productHandlerHelpers.ts
+++ client/src/features/productHandlerHelpers.ts
@@ -1,14 +1,10 @@
 import { IUserResponse } from '../types';
-import { addToList } from './productListInputHandler';
+import createElemWithClass from '../helpers/createElementWithClass';
 
-export const deleteProduct = (e: Event): void => {
-  const target = e.target as HTMLElement;
-  const targetProduct = target.closest('.products-list__item') as HTMLElement;
-  const parentList = target.closest('.products-list') as HTMLElement;
-  const productName = (targetProduct.querySelector('span') as HTMLElement).innerText.toLowerCase().trim();
-
+export const deleteUserProduct = (targetProduct: HTMLElement, parentList: HTMLElement, productName: string): void => {
   parentList.removeChild(targetProduct);
   checkIfEmpty(parentList);
+
   const userObject = JSON.parse(localStorage.getItem('user') || 'null') as IUserResponse;
   if (userObject?.products) {
     if (parentList.classList.contains('products-list_stocked')) {
@@ -21,47 +17,27 @@ export const deleteProduct = (e: Event): void => {
       }
     }
   }
+
   localStorage.setItem('user', JSON.stringify(userObject));
 };
 
-export const moveToStockedHandler = (e: Event): void => {
-  const shoppingList = document.querySelector('.products-list_needed') as HTMLElement;
+export const moveToStockedHandler = (
+  targetProduct: HTMLElement,
+  productName: string,
+  dropdownProductsCurrent: string[],
+  dropdownProductsNext: string[]
+): void => {
   const stockedList = document.querySelector('.products-list_stocked') as HTMLElement;
-  const target = e.target as HTMLElement;
-  const targetProduct = target.closest('.products-list__item') as HTMLElement;
-  const productName = (targetProduct.querySelector('span') as HTMLElement).innerText.toLowerCase().trim();
+  const userObject = JSON.parse(localStorage.getItem('user') || 'null') as IUserResponse;
 
-  const stockedProductNameElems = stockedList.querySelectorAll('.product');
-  const stockedProductNames: string[] = [];
-  stockedProductNameElems.forEach(elem => {
-    stockedProductNames.push((elem as HTMLElement).innerText.trim().toLowerCase());
-  });
-  if (stockedProductNames.includes(productName)) {
-    shoppingList?.removeChild(targetProduct);
-    checkIfEmpty(shoppingList);
-  } else {
-    shoppingList?.removeChild(targetProduct);
-    addToList(productName, stockedList);
-
-    checkIfEmpty(shoppingList);
-    checkIfEmpty(stockedList);
-
-    const userObject = JSON.parse(localStorage.getItem('user') || 'null') as IUserResponse;
-
-    if (userObject?.products) {
-      if (!userObject.products.own.includes(productName)) {
-        userObject.products.own.push(productName);
-        userObject.products.shopping = userObject.products.shopping.filter(str => str !== productName);
-      }
-    } else {
-      userObject.products = { shopping: [], own: [] };
-      userObject.products.own.push(productName);
-    }
-    localStorage.setItem('user', JSON.stringify(userObject));
+  if (!userObject.products.own.includes(productName)) {
+    addProductToList(productName, stockedList, dropdownProductsNext, dropdownProductsCurrent);
+    addProductToLStorage(productName, 'own');
+    deleteProductFromDropdown(productName, dropdownProductsNext);
   }
 };
 
-export const filterOptions = (options: string[], value: string): string[] => {
+export const filterIncludedOptions = (options: string[], value: string): string[] => {
   return options.filter(item => item.includes(value));
 };
 
@@ -72,3 +48,106 @@ export const checkIfEmpty = (list: HTMLElement): void => {
     list?.querySelector('.message')?.classList.add('hidden');
   }
 };
+
+export function renderDropdownItems(list: HTMLElement, dropdownList: string[]): void {
+  list.innerHTML = '';
+
+  dropdownList.forEach(item => {
+    const listElem = createElemWithClass('li', 'product-options__item');
+    listElem.id = item;
+    listElem.innerText = item;
+    list.append(listElem);
+  });
+}
+
+export function addProductToList(
+  product: string,
+  list: HTMLElement,
+  dropdownProductsCurrent: string[],
+  dropdownProductsNext: string[]
+): void {
+  const listItem = createProductListItem(product, dropdownProductsCurrent, dropdownProductsNext);
+  list.append(listItem);
+  checkIfEmpty(list);
+}
+
+function createProductListItem(
+  product: string,
+  dropdownProductsCurrent: string[],
+  dropdownProductsNext: string[]
+): HTMLElement {
+  const listItem = createElemWithClass('li', 'products-list__item');
+  listItem.innerHTML = `
+    <span class="product">${product}</span> 
+    <div class="product__controls product-controls"> 
+      <div class="product-controls__add-btn"></div> 
+      <div class="product-controls__delete-btn"></div>
+    </div> 
+  `;
+
+  listItem.querySelector('.product-controls__delete-btn')?.addEventListener('click', e => {
+    const parentList = listItem.closest('.products-list') as HTMLElement;
+    const target = e.target as HTMLElement;
+    const targetProduct = target.closest('.products-list__item') as HTMLElement;
+    const productName = (targetProduct.querySelector('span') as HTMLElement).innerText.trim().toLowerCase();
+
+    deleteUserProduct(targetProduct, parentList, productName);
+    addProductToDropdown(productName, dropdownProductsCurrent);
+  });
+
+  listItem.querySelector('.product-controls__add-btn')?.addEventListener('click', e => {
+    const parentList = listItem.closest('.products-list') as HTMLElement;
+    const target = e.target as HTMLElement;
+    const targetProduct = target.closest('.products-list__item') as HTMLElement;
+    const productName = (targetProduct.querySelector('span') as HTMLElement).innerText.trim().toLowerCase();
+
+    moveToStockedHandler(targetProduct, productName, dropdownProductsCurrent, dropdownProductsNext);
+    deleteUserProduct(targetProduct, parentList, productName);
+    addProductToDropdown(productName, dropdownProductsCurrent);
+  });
+
+  return listItem;
+}
+
+export function renderListsFromLStorage(dropdownProductsStocked: string[], dropdownProductsNeeded: string[]) {
+  const userObject = JSON.parse(localStorage.getItem('user') || 'null') as IUserResponse;
+  const shoppingList = document.querySelector('.products-list_needed') as HTMLElement;
+  const stockedList = document.querySelector('.products-list_stocked') as HTMLElement;
+
+  if (userObject?.products) {
+    userObject.products.own?.forEach(product => {
+      deleteProductFromDropdown(product, dropdownProductsStocked);
+      const listItem = createProductListItem(product, dropdownProductsStocked, dropdownProductsNeeded);
+      stockedList.append(listItem);
+    });
+    userObject.products.shopping?.forEach(product => {
+      deleteProductFromDropdown(product, dropdownProductsNeeded);
+      const listItem = createProductListItem(product, dropdownProductsNeeded, dropdownProductsStocked);
+      shoppingList.append(listItem);
+    });
+  }
+
+  checkIfEmpty(shoppingList);
+  checkIfEmpty(stockedList);
+}
+
+export function addProductToLStorage(str: string, listName: 'own' | 'shopping'): void {
+  const userObject = JSON.parse(localStorage.getItem('user') || 'null') as IUserResponse;
+  if (userObject?.products) {
+    userObject.products[listName].push(str);
+    localStorage.setItem('user', JSON.stringify(userObject));
+  }
+}
+
+export function addProductToDropdown(productName: string, dropdownList: string[]) {
+  if (!dropdownList.includes(productName)) {
+    dropdownList.push(productName);
+    dropdownList.sort();
+  }
+}
+
+export function deleteProductFromDropdown(productName: string, dropdownList: string[]) {
+  const productIndex = dropdownList.findIndex(elementName => elementName === productName.toLowerCase());
+  const deleteCount = productIndex === -1 ? 0 : 1;
+  dropdownList.splice(productIndex, deleteCount);
+}
--- client/src/features/productListControlsHandler.ts
+++ client/src/features/productListControlsHandler.ts
@@ -1,15 +1,24 @@
-import { deleteProduct, moveToStockedHandler } from './productHandlerHelpers';
+import { deleteUserProduct, moveToStockedHandler } from './productHandlerHelpers';
 
 const productListControlsHandler = (): void => {
   const addButtonsList = document.querySelectorAll('.product-controls__add-btn');
   const deleteButtonsList = document.querySelectorAll('.product-controls__delete-btn');
 
   addButtonsList.forEach(btn => {
-    btn.addEventListener('click', e => moveToStockedHandler(e));
+    btn.addEventListener('click', e => {
+      console.log('add button')
+      // moveToStockedHandler(e)
+    });
   });
 
   deleteButtonsList.forEach(btn => {
-    btn.addEventListener('click', e => deleteProduct(e));
+    const parentList = btn.closest('.products-list') as HTMLElement;
+    // const target = e.target as HTMLElement;
+    const targetProduct = btn.closest('.products-list__item') as HTMLElement;
+    const productName = (targetProduct.querySelector('span') as HTMLElement).innerText.trim().toLowerCase();
+    btn.addEventListener('click', e => {
+      deleteUserProduct(targetProduct, parentList, productName);
+    });
   });
 };
 
